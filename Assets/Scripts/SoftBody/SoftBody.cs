074c810757c3574c46fa80bcfbaae2fbb47a5129
Gorynych_0 <25215voe@mail.ru>
2024-11-02 14:39:02 +0300

    Penguin

--- Assets/Scripts/SoftBody/SoftBody.cs
+++ Assets/Scripts/SoftBody/SoftBody.cs
@@ -1,37 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.XR;
+using static UnityEngine.GraphicsBuffer;
 
 public class SoftBody : MonoBehaviour
 {
-    [SerializeField] private List<Rigidbody2D> boneList;
-    [SerializeField] SpringSettings springSettings;
+    [SerializeField] private Rigidbody2D midBone;
+    [SerializeField, Tooltip("Работает только для чётных количеств")] private List<Rigidbody2D> boneList;
+    [SerializeField] private float frequency;
+    [SerializeField] private float distance;
 
-    private void Awake()
+    /*private void Update()
     {
-        CreateBoneSprings();
+        boneList.ForEach(bone => {
+            Vector3 Look = bone.transform.InverseTransformPoint(midBone.transform.position);
+            float Angle = Mathf.Atan2(Look.y, Look.x) * Mathf.Rad2Deg;
+            bone.transform.Rotate(0, 0, Angle);
+        });
+    }*/
+
+    private void SettingSpring(SpringJoint2D spring, Rigidbody2D body)
+    {
+        spring.connectedBody = body;
+        spring.frequency = frequency;
+        //spring.autoConfigureDistance = false;
+        //spring.distance = distance;
     }
 
-    // работает только для чётных количеств
-    public void CreateBoneSprings()
+    public void CreateMidBoneSprings()
     {
         for (int i = 0; i < boneList.Count; i++)
         {
             var bone = boneList[i];
 
+            var oldSprings = bone.GetComponents<Joint2D>();
+            foreach (var item in oldSprings)
+            {
+                DestroyImmediate(item);
+            }
+
             var leftSpring = bone.gameObject.AddComponent<SpringJoint2D>();
             var rightSpring = bone.gameObject.AddComponent<SpringJoint2D>();
             var opositeSpring = bone.gameObject.AddComponent<SpringJoint2D>();
 
-            leftSpring.connectedBody = i == 0 ? boneList[boneList.Count - 1] : boneList[i - 1];
-            rightSpring.connectedBody = i == boneList.Count - 1 ? boneList[i] : boneList[i - 1];
-            opositeSpring.connectedBody = i >= boneList.Count / 2 ? boneList[i - boneList.Count / 2] : boneList[i + boneList.Count / 2];
+            SettingSpring(leftSpring, i == 0 ? boneList[boneList.Count - 1] : boneList[i - 1]);
+            SettingSpring(rightSpring, i == boneList.Count - 1 ? boneList[0] : boneList[i + 1]);
+            SettingSpring(opositeSpring, midBone);
+        }
+    }
+}
 
+[CustomEditor(typeof(SoftBody))]
+[CanEditMultipleObjects]
+class DecalMeshHelperEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        var script = target as SoftBody;
 
-            leftSpring.frequency = springSettings.Frequency;
-            rightSpring.frequency = springSettings.Frequency;
-            opositeSpring.frequency = springSettings.Frequency;
+        if (GUILayout.Button("Generate springs"))
+        {
+            script.CreateMidBoneSprings();
         }
     }
 }
