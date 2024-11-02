af3408869fcd3b109ee234298db39d23b62c445e
Stepan Gurbashkov <79417613+Chekhov973@users.noreply.github.com>
2024-11-02 18:26:18 +0300

    Feat: touchable enemy script

---
+++ Assets/Scripts/Enemies/TouchEnemy.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchEnemy : MonoBehaviour
+{
+    [SerializeField] private float _damage;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out Player target))
+        {
+            target.GetDamage(_damage);
+        }
+    }
+}
