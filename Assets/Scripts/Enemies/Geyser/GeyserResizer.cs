3fcaf57be1a99419bc03bac2c02a2d1650baea5c
Gorynych_0 <25215voe@mail.ru>
2024-11-04 14:11:15 +0300

    Geyser finish

--- Assets/Scripts/Enemies/Geyser/GeyserResizer.cs
+++ Assets/Scripts/Enemies/Geyser/GeyserResizer.cs
@@ -1,26 +1,72 @@
 using DG.Tweening;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GeyserResizer : MonoBehaviour
 {
+    [SerializeField] Transform gayserTransform;
+
     [SerializeField] private float upTime;
     [SerializeField] private float inUpPositionTime;
     [SerializeField] private float inDownPositionTime;
 
+    [SerializeField] private List<Sprite> sprites;
+    private IEnumerator<Sprite> spriteEnumerator;
+
     private void Start()
     {
-        transform.parent.DOLocalMoveY(-transform.lossyScale.y / 2, 0);
+        gayserTransform.parent.DOLocalMoveY(-gayserTransform.localScale.y / 2, 0);
+        spriteEnumerator = GetSpriteEnumerator();
     }
 
     public void StartLoop()
     {
-        DOTween.Sequence()
-            .Append(transform.parent.DOLocalMoveY(transform.lossyScale.y / 2, upTime).SetEase(Ease.OutQuad))
-            .AppendInterval(inUpPositionTime)
-            .Append(transform.parent.DOLocalMoveY(-transform.lossyScale.y / 2, upTime).SetEase(Ease.InQuad))
-            .AppendInterval(inDownPositionTime)
+        var s = DOTween.Sequence();
+
+        for (int i = 0; i < sprites.Count; i++)
+        {
+            s.AppendCallback(SetSprite)
+                .Append(gayserTransform.parent.DOLocalMoveY(i * gayserTransform.localScale.y / 2 / (sprites.Count - 1), upTime / sprites.Count).SetEase(Ease.Linear));
+        }
+
+        s.AppendInterval(inUpPositionTime);
+
+        for (int i = 0; i < sprites.Count; i++)
+        {
+            s.AppendCallback(SetSprite)
+                .Append(gayserTransform.parent.DOLocalMoveY(i * -gayserTransform.localScale.y / 2 / (sprites.Count - 1), upTime / sprites.Count).SetEase(Ease.Linear));
+        }
+
+        s.AppendInterval(inDownPositionTime)
+            .AppendCallback(ResetEnumerator)
             .SetLoops(-1);
+    }
+
+    private void ResetEnumerator()
+    {
+        spriteEnumerator = GetSpriteEnumerator();
+    }
+
+    private void SetSprite()
+    {
+        spriteEnumerator.MoveNext();
+        var sprite = spriteEnumerator.Current;
+        gayserTransform.parent.GetComponentInChildren<SpriteRenderer>().sprite = sprite;
+    }
+
+    private IEnumerator<Sprite> GetSpriteEnumerator()
+    {
+        for (int i = 0; i < sprites.Count; i++)
+        {
+            yield return sprites[i];
+        }
+
+        for (int i = 0; i < sprites.Count; i++)
+        {
+            yield return sprites[sprites.Count - i - 1];
+        }
     }
 }
