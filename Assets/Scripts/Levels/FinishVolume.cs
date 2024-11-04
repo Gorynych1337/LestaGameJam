718f71154ac43dfa2bd02a012046d13f0f29e33d
Vamiro <Vovka00821@yandex.ru>
2024-11-04 17:37:00 +0300

    - finish to menu

--- Assets/Scripts/Levels/FinishVolume.cs
+++ Assets/Scripts/Levels/FinishVolume.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishVolume : MonoBehaviour
 {
@@ -9,5 +10,6 @@ public class FinishVolume : MonoBehaviour
     {
         if (!other.CompareTag("Player")) return;
         GameManager.Instance.FinishLevel();
+        SceneManager.LoadScene(0);
     }
 }
